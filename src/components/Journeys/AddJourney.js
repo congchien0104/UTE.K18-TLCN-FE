e79842b85f001849d983322de9a39d3c25a206e7
hdt2401 <thien12000@gmail.com>
2022-06-12 16:33:18 +0700

    update journey form

--- src/components/Journeys/AddJourney.js
+++ src/components/Journeys/AddJourney.js
@@ -5,20 +5,21 @@ import * as Yup from "yup";
 import { useParams, useHistory } from "react-router-dom";
 import companyService from "../../services/company.service";
 import { SuccessNotify } from "../../utils/Notify";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const temp = [
-    {
-        time: '11:00:00',
-        address: 'Eahleo'
-    },
-    {
-        time: '11:00:00',
-        address: 'Eahleo'
-    },
-    {
-        time: '11:00:00',
-        address: 'Eahleo'
-    },
+  {
+    time: '11:00:00',
+    address: 'Eahleo'
+  },
+  {
+    time: '11:00:00',
+    address: 'Eahleo'
+  },
+  {
+    time: '11:00:00',
+    address: 'Eahleo'
+  },
 ];
 
 function AddJourney() {
@@ -39,20 +40,20 @@ function AddJourney() {
 
   const handleAddressTo = () => {
     const dataTo = {
-        time_hour: '',
-        address: '',
-        status: false,
-        lineId: id,
+      time_hour: '',
+      address: '',
+      status: false,
+      lineId: id,
     }
     dataSave.push(dataTo);
   }
 
   const handleAddressDes = () => {
     const dataDes = {
-        time_hour: '',
-        address: '',
-        status: true,
-        lineId: id,
+      time_hour: '',
+      address: '',
+      status: true,
+      lineId: id,
     }
     dataSave.push(dataDes);
   }
@@ -60,118 +61,195 @@ function AddJourney() {
   function onSubmit(data) {
     console.log("dkm", data);
     companyService.createJourney(id, data)
-    .then((res) => {
+      .then((res) => {
         console.log(res.data);
         SuccessNotify("Tạo Hanh Trinh Thành Công");
         history.push(`/company/cars/line/${res.data.data.id}`);
-    })
-    .catch((e) => {
+      })
+      .catch((e) => {
         console.log(e);
-    });
+      });
   }
   return (
-    <div className="container mt-5">
+    <div className="container">
       <h2>Tạo Hành Trình</h2>
-      <div class="row g-3">
-        <div class="col">
-            {
-                temp.map((item) => (
-                    <label>Thời Gian: {item.time} - {item.address}</label>
-                ))
-            }
+      {/* <div className="row">
+        <div className="col-6">
+          {
+            temp.map((item) => (
+              <label>Thời Gian: {item.time} - {item.address}</label>
+            ))
+          }
         </div>
-        <div class="col">
-            {
-                temp.map((item) => (
-                    <label>Thời Gian: {item.time} - {item.address}</label>
-                ))
-            }
+        <div className="col-6">
+          {
+            temp.map((item) => (
+              <label>Thời Gian: {item.time} - {item.address}</label>
+            ))
+          }
         </div>
       </div>
-      <div class="row g-3 mt-5">
-        <div class="col">
+      <div className="row">
+        <div className="col-6">
+          <label for="time_hour">Thời Gian</label>
+          <input
+            name="time_hour"
+            type="time"
+            {...register("time_hour")}
+            className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
+          />
+          <div className="invalid-feedback">{errors.time_hour?.message}</div>
+        </div>
+        <div className="col-6">
+          <label for="time_hour">Thời Gian</label>
+          <input
+            name="time_hour"
+            type="time"
+            {...register("time_hour")}
+            className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
+          />
+          <div className="invalid-feedback">{errors.time_hour?.message}</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <label for="station">Địa chỉ</label>
+          <input
+            name="address"
+            type="text"
+            {...register("address")}
+            className={`form-control ${errors.address ? "is-invalid" : ""}`}
+          />
+          <div className="invalid-feedback">{errors.address?.message}</div>
+        </div>
+        <div className="col">
+          <label for="station">Địa chỉ</label>
+          <input
+            name="address"
+            type="text"
+            {...register("address")}
+            className={`form-control ${errors.address ? "is-invalid" : ""}`}
+          />
+          <div className="invalid-feedback">{errors.address?.message}</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-primary mt-2" onClick={handleAddressTo}>
+            Thêm
+          </button>
+        </div>
+        <div className="col">
+          <button className="btn btn-primary mt-2" onClick={handleAddressDes}>
+            Thêm
+          </button>
+        </div>
+      </div> */}
+      <div className="row">
+        <div className="col-6 journey-start">
+          <div className="card">
+            <div className="card-body">
+              <div className="journey-block">
                 <label for="time_hour">Thời Gian</label>
                 <input
-                    name="time_hour"
-                    type="time"
-                    {...register("time_hour")}
-                    className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
+                  name="time_hour"
+                  type="time"
+                  {...register("time_hour")}
+                  className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
                 />
                 <div className="invalid-feedback">{errors.time_hour?.message}</div>
-        </div>
-        <div class="col">
-                <label for="time_hour">Thời Gian</label>
+              </div>
+              <div className="journey-block mb-4">
+                <label for="station">Địa chỉ</label>
                 <input
-                    name="time_hour"
-                    type="time"
-                    {...register("time_hour")}
-                    className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
+                  name="address"
+                  type="text"
+                  {...register("address")}
+                  className={`form-control ${errors.address ? "is-invalid" : ""}`}
                 />
-                <div className="invalid-feedback">{errors.time_hour?.message}</div>
-        </div>
-      </div>
-      <div class="row g-3 mt-2">
-        <div class="col">
-            <label for="station">Địa chỉ</label>
-            <input
-              name="address"
-              type="text"
-              {...register("address")}
-              className={`form-control ${errors.address ? "is-invalid" : ""}`}
-            />
-            <div className="invalid-feedback">{errors.address?.message}</div>
-        </div>
-        <div class="col">
-            <label for="station">Địa chỉ</label>
-            <input
-              name="address"
-              type="text"
-              {...register("address")}
-              className={`form-control ${errors.address ? "is-invalid" : ""}`}
-            />
-            <div className="invalid-feedback">{errors.address?.message}</div>
-        </div>
-      </div>
-      <div class="row g-3 mt-2">
-        <div class="col">
-            <button className="btn btn-primary mt-2" onClick={handleAddressTo}>
-                Thêm
-            </button>
-        </div>
-        <div class="col">
-            <button className="btn btn-primary mt-2" onClick={handleAddressDes}>
-                Thêm
-            </button>
-        </div>
-      </div>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <div class="row">
-            <div class="col">
-                <div>
-                    <label for="time_hour">Thời Gian</label>
-                    <input
-                    name="time_hour"
-                    type="time"
-                    {...register("time_hour")}
-                    className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
-                    />
-                    <div className="invalid-feedback">{errors.time_hour?.message}</div>
-                </div>
-                <div>
-                    <label for="time_hour">Thời Gian</label>
-                    <input
-                    name="time_hour"
-                    type="time"
-                    {...register("time_hour")}
-                    className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
-                    />
-                    <div className="invalid-feedback">{errors.time_hour?.message}</div>
-                </div>
+                <div className="invalid-feedback">{errors.address?.message}</div>
+              </div>
+              <div className="journey-block">
+                <button className="btn btn-primary" onClick={handleAddressTo}>
+                  Thêm điểm đón
+                </button>
+              </div>
             </div>
+          </div>
+          <div className="journey-list">
+            {
+              temp.map((item) => (
+                <p className="text-mute fs-5">Thời Gian: {item.time} - {item.address}</p>
+              ))
+            }
+          </div>
         </div>
-        <div class="row mt-2">
-          <div class="col">
+        <div className="col-6 journey-end">
+          <div className="card">
+            <div className="card-body">
+              <div className="journey-block">
+                <label for="time_hour">Thời Gian</label>
+                <input
+                  name="time_hour"
+                  type="time"
+                  {...register("time_hour")}
+                  className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
+                />
+                <div className="invalid-feedback">{errors.time_hour?.message}</div>
+              </div>
+              <div className="journey-block mb-4">
+                <label for="station">Địa chỉ</label>
+                <input
+                  name="address"
+                  type="text"
+                  {...register("address")}
+                  className={`form-control ${errors.address ? "is-invalid" : ""}`}
+                />
+                <div className="invalid-feedback">{errors.address?.message}</div>
+              </div>
+              <div className="journey-block">
+                <button className="btn btn-primary" onClick={handleAddressDes}>
+                  Thêm điểm trả
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="journey-list">
+            {
+              temp.map((item) => (
+                <p className="text-mute fs-5">Thời Gian: {item.time} - {item.address}</p>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* <div className="row">
+          <div className="col">
+            <div>
+              <label for="time_hour">Thời Gian</label>
+              <input
+                name="time_hour"
+                type="time"
+                {...register("time_hour")}
+                className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
+              />
+              <div className="invalid-feedback">{errors.time_hour?.message}</div>
+            </div>
+            <div>
+              <label for="time_hour">Thời Gian</label>
+              <input
+                name="time_hour"
+                type="time"
+                {...register("time_hour")}
+                className={`form-control ${errors.time_hour ? "is-invalid" : ""}`}
+              />
+              <div className="invalid-feedback">{errors.time_hour?.message}</div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
             <label for="station">Địa chỉ</label>
             <input
               name="address"
@@ -181,8 +259,8 @@ function AddJourney() {
             />
             <div className="invalid-feedback">{errors.address?.message}</div>
           </div>
-        </div>
-        <button type="submit" className="btn btn-primary mt-2">
+        </div> */}
+        <button type="submit" className="btn btn-primary">
           Lưu
         </button>
       </form>
